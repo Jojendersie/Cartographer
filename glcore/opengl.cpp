78ec8af3ada3ed05c5faba94d1006b12bb71f06a
Johannes Jendersie <jojendersie@web.de>
2015-05-25 11:16:01 +0200

    GL-sided error handler

---
+++ glcore/opengl.cpp
@@ -0,0 +1,54 @@
+#include "core/error.hpp"
+#include "opengl.hpp"
+
+namespace Ca {
+		
+	bool GLError(const char*  _openGLFunctionName)
+	{
+		GLenum Error = glGetError();
+		if (Error != GL_NO_ERROR)
+		{
+			const char* errorString;
+			const char* description;
+			switch (Error)
+			{
+			case GL_INVALID_ENUM:
+				errorString = "GL_INVALID_ENUM";
+				description = "An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_INVALID_VALUE:
+				errorString = "GL_INVALID_VALUE";
+				description = "A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_INVALID_OPERATION:
+				errorString = "GL_INVALID_OPERATION";
+				description = "The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_INVALID_FRAMEBUFFER_OPERATION:
+				errorString = "GL_INVALID_FRAMEBUFFER_OPERATION";
+				description = "The command is trying to render to or read from the framebuffer while the currently bound framebuffer is not framebuffer complete. "
+					"The offending command is ignored and has no other side effect than to set the error flag.";
+				break;
+
+			case GL_OUT_OF_MEMORY:
+				errorString = "GL_OUT_OF_MEMORY";
+				description = "There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.";
+				break;
+
+			default:
+				errorString = "UNKNOWN";
+				description = "Unknown error code.";
+				break;
+			}
+			s_onError((std::string("OpenGL Error during ") + _openGLFunctionName + ": " + errorString + "(" + description + ")").c_str());
+			return true;
+		}
+
+		// No error
+		return false;
+	}
+	
+} // namespace Ca
---
+++ glcore/opengl.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+namespace Ca {
+	
+	/// Check OpenGL for an error and report it if necessary.
+	/// \details This call may stall the CPU/GPU due to glGetError().
+	/// \returns true if an error occurred.
+	bool GLError(const char*  _openGLFunctionName);
+	
+//#define NAME_OF(GLFunction) (#GLFunction)
+	
+	/// OpenGL call with additional checks.
+	template<typename ReturnType, typename T...>
+	ReturnType glCall(ReturnType (_function*)(T...), T... _args)
+	{
+		auto ret = _function(_args);
+#ifdef DEBUG
+		GLError(#GLFunction);
+#endif
+		return ret;
+	}
+
+	/// No return overload of OpenGL call with additional checks.
+	template<typename T...>
+	void glCall(void (_function*)(T...), T... _args)
+	{
+		_function(_args);
+#ifdef DEBUG
+		GLError(#GLFunction);
+#endif
+	}
+	
+} // namespace Ca
