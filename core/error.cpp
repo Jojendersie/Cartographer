a88596c22f4b9f2b710fdf74320cbb0029637cbb
Johannes Jendersie <jojendersie@web.de>
2015-05-25 11:15:30 +0200

    CPU sided error handler

---
+++ core/error.cpp
@@ -0,0 +1,23 @@
+#include "error.hpp"
+
+namespace Ca {
+	
+	void defaultHandler(const char* _message)
+	{
+		std::cerr << _message << std::cerr;
+	}
+	
+	static void (s_onError*)(const char* _message) = defaultHandler;
+	
+	void setErrorCallback(void (_onError*)(const char* _message))
+	{
+		s_onError("Switching error handler.");
+		s_onError = _onError;
+	}
+	
+	void error(const char* _message)
+	{
+		s_onError(_message);
+	}
+		
+} // namespace Ca
