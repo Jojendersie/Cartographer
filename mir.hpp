974118101309c045ec453b9a3688069d89a37a29
Johannes Jendersie <jojendersie@web.de>
2015-06-21 15:05:04 +0200

    engine header file and gitignore

---
+++ mir.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "core/error.hpp"
+#include "core/scopedptr.hpp"
+#include "glcore/device.hpp"
+#include "renderer/instancerenderer.hpp"
+#include "renderer/spriterenderer.hpp"
