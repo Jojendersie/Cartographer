1e175681411c7a014fb15fbe388c40ac65e9159e
Johannes Jendersie <jojendersie@web.de>
2015-11-06 12:30:02 +0100

    a new project with lots of files as structural proposal

---
+++ cagui.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace cag {
+
+	/// Manages global properties like look-and-feel and forwards input to the GUIs.
+	class GUIManagar
+	{
+	};
+
+} // namespace cag
---
+++ properties/anchorable.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+namespace cag {
+} // namespace cag
