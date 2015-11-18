1591b03640b6d96bfaa1484e83dc43a6d157bc9a
Johannes Jendersie <jojendersie@web.de>
2015-11-18 16:49:04 +0100

    implemented RefFrame::isMouseOver

--- src/properties/refframe.cpp
+++ src/properties/refframe.cpp
@@ -4,4 +4,11 @@
 
 namespace cag {
 
+	bool RefFrame::isMouseOver(const Coord& _mousePos)
+	{
+		// Simple rectangle test if the coordinate is inside
+		return _mousePos.x >= left() && _mousePos.x <= right()
+			&& _mousePos.y >= bottom() && _mousePos.y <= top();
+	}
+
 } // namespace cag
