3a563b3bffee64745e9339536e2dab13ff3bdb36
Johannes Jendersie <jojendersie@web.de>
2015-11-24 14:04:36 +0100

    new cagui top level header to include entire library (completes the last commit)

---
+++ include/cagui.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "guimanager.hpp"
+#include "widgets/button.hpp"
+#include "widgets/chechbox.hpp"
+#include "widgets/colorpicker.hpp"
+#include "widgets/directionpicker.hpp"
+#include "widgets/dropdownbox.hpp"
+#include "widgets/editbox.hpp"
+#include "widgets/frame.hpp"
+#include "widgets/multilineeditbox.hpp"
+#include "widgets/slider.hpp"
+#include "widgets/treelist.hpp"
+
+#include "backend/charcoal.hpp"
