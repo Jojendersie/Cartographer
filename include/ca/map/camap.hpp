74d63506bb075dae24b7f45bb55ee59eed6dd124
Johannes Jendersie <jojendersie@web.de>
2016-12-28 19:15:41 +0100

    high level include file

---
+++ include/ca/map/camap.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "ca/map/action/input.hpp"
+
+#include "ca/map/map/tile.hpp"
+#include "ca/map/map/construct.hpp"
+#include "ca/map/map/cell.hpp"
+#include "ca/map/map/grid.hpp"
