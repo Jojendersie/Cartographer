186dfdce1c86607cb393df9a53de6382d13c44da
Johannes Jendersie <jojendersie@web.de>
2017-01-11 10:51:45 +0100

    removed a few syntax errors * removed the occupancy from tiles

--- include/ca/map/map/tile.hpp
+++ include/ca/map/map/tile.hpp
@@ -1,21 +1,17 @@
 #pragma once
 
-#include "ca/action/input.hpp"
+#include <ei/vector.hpp>
+#include "ca/map/action/input.hpp"
 
 namespace ca { namespace map {
-	
-	enum class Occupancy
-	{
-		AMBIENT,	// Counts like unoccupyed cells
-		OCCUPIED,	// Occupied but still pass able
-		SOLID,		// Non pass able
-	};
 	
 	/// A tile is the smallest logical unit on each map.
 	/// It may have some graphical representation, an input action and unit triggers.
 	class Tile
 	{
 	public:
+		virtual ~Tile() = default;
+
 		virtual void draw() const {}
 		/// \param [in] _screenCoord An [0,1]^2 position on screen.
 		virtual void processInput(const ei::Vec2& _screenCoord, MouseAction _mouseAction, float _mouseActAmount) {}
@@ -26,10 +22,8 @@ namespace ca { namespace map {
 		virtual void trigger(int _neighbor, Unit* _unit) {}
 		
 		/// Solid objects block paths of units.
-		bool isSolid() const { return m_occupancy == Occupancy::SOLID; }
-		bool isOccupied() const { return (m_occupancy == Occupancy::SOLID) || (m_occupancy == Occupancy::OCCUPIED); }
+		virtual bool isSolid() const = 0;
 	protected:
-		Occupancy m_occupancy;	///< Each tile should be occupied or solid. None is reserved for empty cells.
 	};
 	
 }} // namespace ca::map
