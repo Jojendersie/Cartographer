d57edf4074b392dcee8db67833d98b477b15c939
Johannes Jendersie <jojendersie@web.de>
2017-01-13 10:29:14 +0100

    generic buffer class

--- include/ca/charcoal.hpp
+++ include/ca/charcoal.hpp
@@ -6,6 +6,7 @@
 #include "cc/glcore/shader.hpp"
 #include "cc/glcore/sampler.hpp"
 #include "cc/glcore/texture.hpp"
+#include "cc/glcore/buffer.hpp"
 #include "cc/renderer/instancerenderer.hpp"
 #include "cc/renderer/spriterenderer.hpp"
 #include "cc/renderer/fontrenderer.hpp"
