bb65a9fdb17329e08a331da336630214b64b6727
Johannes Jendersie <jojendersie@web.de>
2017-02-09 09:36:21 +0100

    added high resolution clock

--- include/ca/paper.hpp
+++ include/ca/paper.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "pa/log.hpp"
+#include "pa/highresclock.hpp"
 #include "pa/memory/refptr.hpp"
 #include "pa/containers/hashmap.hpp"
---
+++ src/highresclock.cpp
@@ -0,0 +1,55 @@
+#include "highresclock.hpp"
+
+HRClock::HRClock() : m_timeStamp(now())
+{
+}
+
+double HRClock::deltaTime()
+{
+	double oldTime = m_timeStamp;
+	m_timeStamp = now();
+	return m_timeStamp - oldTime;
+}
+
+// ********** Platform dependent code *************
+#if defined(_WIN32) || defined(WIN32)
+
+#include <windows.h>
+
+const double QUERY_FREQUENCY = []() -> double
+{
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	return frequency.QuadPart / 1000.0;
+}();
+const long long START_TIMEPOINT = []() -> long long
+{
+	LARGE_INTEGER count;
+    QueryPerformanceCounter(&count);
+	return count.QuadPart;
+}();
+
+double HRClock::now()
+{
+	LARGE_INTEGER count;
+    QueryPerformanceCounter(&count);
+	return (count.QuadPart - START_TIMEPOINT) / QUERY_FREQUENCY;
+}
+
+#else
+
+#include <chrono>
+
+const std::chrono::high_resolution_clock::time_point START_TIMEPOINT = []()
+{
+	return std::chrono::high_resolution_clock::now();
+}();
+
+double HRClock::now()
+{
+	auto time = std::chrono::high_resolution_clock::now();
+	std::chrono::nanoseconds delta = time - START_TIMEPOINT;
+	return delta.count() / 1000000.0;
+}
+
+#endif
