57fc1090c410e5fc5723285479a2268679799f52
Johannes Jendersie <jojendersie@web.de>
2023-03-12 18:00:49 +0100

    slightly improved rendering performance of node handles

--- src/rendering/sharp3dtheme.cpp
+++ src/rendering/sharp3dtheme.cpp
@@ -193,16 +193,24 @@ namespace ca { namespace gui {
 		Vec4 centerColor(_color * 2.0f, 1.0f);
 		Vec4 color(_color / 2.0f, 1.0f);
 		// Draw a triangle fan to create a small circle with a single other-colored vertex in the center
+		constexpr float COS_A = 0.866025404f; 	// cos(2*PI/12.0f)
+		constexpr float SIN_A = 0.5f;			// sin(2*PI/12.0f)
 		Triangle2D triangle;
 		triangle.v0 = _position + _radius / 3.0f * Coord2(cos(2*PI*3/12.0f), sin(2*PI*3/12.0f));
-		triangle.v2 = _position + _radius * Coord2(cos(2*PI/12.0f), sin(2*PI/12.0f));
+		triangle.v2 = _position + _radius * Coord2(COS_A, SIN_A);
+		// Using addition theorems for improved performance
+		float cosA = COS_A;
+		float sinA = SIN_A;
 		for(int i = 0; i < 12; ++i)
 		{
+			const float newSinA = sinA * COS_A + cosA * SIN_A;
+			const float newCosA = cosA * COS_A - sinA * SIN_A;
+			sinA = newSinA;
+			cosA = newCosA;
 			triangle.v1 = triangle.v2;
-			triangle.v2 = _position + _radius * Coord2(cos(2*PI * (i+2)/12.0f), sin(2*PI * (i+2)/12.0f));
+			triangle.v2 = _position + _radius * Coord2(cosA, sinA);
 			GUIManager::renderBackend().drawTriangle(triangle, centerColor, color, color);
 		}
-		// TODO: draw a border
 	}
 
 	void Sharp3DTheme::drawLine(const ei::Vec2* _positions, int _numPositions, const ei::Vec4& _colorA, const ei::Vec4& _colorB)
